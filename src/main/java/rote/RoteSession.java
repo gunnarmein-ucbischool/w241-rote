fd98a1a883d7f659fd9b1f2bdc1fb050f0d9cfbb
gunnarmein-ucbischool <gunnarmein@berkeley.edu>
2020-07-05 10:01:31 -0700

    Initial (not working)

---
+++ src/main/java/rote/RoteSession.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rote;
+
+import spark.Session;
+
+/**
+ *
+ * @author gunnar
+ */
+public class RoteSession {
+    String id = "";
+    Session session;
+    long start;
+
+    public RoteSession(Session s) {
+        this.session = s;
+        this.start = System.currentTimeMillis();
+        this.id = session.id(); // todo; add current data to id
+    }
+    
+    public String getID() {
+        return id;
+    }
+}
