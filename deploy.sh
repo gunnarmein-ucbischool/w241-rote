7bf2380fcaa332063985d9c30f8485eb5469da47
gunnarmein-ucbischool <gunnarmein@berkeley.edu>
2020-07-16 16:51:23 -0700

    changed war file name

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "pw is a AND"
-curl -v -X POST -u "gmein:Powerlifter&1" --data-binary @"target/Rote-1.0-SNAPSHOT.war" https://rote.scm.azurewebsites.net/api/wardeploy
+curl -v -X POST -u "gmein:Powerlifter&1" --data-binary @"target/Rote-1.0.war" https://rote.scm.azurewebsites.net/api/wardeploy
 echo "waiting to deploy ..."
 sleep 40
 curl rote.azurewebsites.net
